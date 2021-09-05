69590617d1dd4d93fefb41b700197fa7eae8d69b
clearsky77 <clearsky3264@gmail.com>
2021-09-05 12:52:26 +0900

    getView 함수 오버라이딩  - convertView 검사(tempRow 변수로 대체)  - 내용물이 없다면, 새로 하나 채워준다.

--- app/src/main/java/com/clearsky77/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/clearsky77/listview_20210905/adapters/StudentAdapter.kt
@@ -1,11 +1,25 @@
 package com.clearsky77.listview_20210905.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.clearsky77.listview_20210905.R
 import com.clearsky77.listview_20210905.datas.StudentData
 
 class StudentAdapter(
     val mContext: Context, //어느 화면에
     val resId: Int, //
     val mList: ArrayList<StudentData> ) : ArrayAdapter<StudentData>(mContext, resId, mList){ //:는 상속이다
+
+    // xml을 가져와서 코틀린단에서 활용하도록 가져와주는 도구 생성
+    val mInflater = LayoutInflater.from(mContext)
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView //임시로 한 줄에 해당하는 것을 만들어 줌.
+        if (tempRow == null){
+            tempRow = mInflater.inflate(R.layout.student_list_item, null) // R이라는 것은 res 폴도다!
+
+        }
+    }
 }
