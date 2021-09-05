e4bc70ee15d5f11c3b3c8bb9f125d90f0755e670
clearsky77 <clearsky3264@gmail.com>
2021-09-05 12:40:03 +0900

    StudentAdapter 받아온 변수를 부모에게 전달하여 해결됨.

--- app/src/main/java/com/clearsky77/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/clearsky77/listview_20210905/adapters/StudentAdapter.kt
@@ -5,7 +5,7 @@ import android.widget.ArrayAdapter
 import com.clearsky77.listview_20210905.datas.StudentData
 
 class StudentAdapter(
-    val mContext: Context,
-    val resId: Int,
-    val mList: ArrayList<StudentData> ) : ArrayAdapter<StudentData>(){ //:는 상속이다
+    val mContext: Context, //어느 화면에
+    val resId: Int, //
+    val mList: ArrayList<StudentData> ) : ArrayAdapter<StudentData>(mContext, resId, mList){ //:는 상속이다
 }
