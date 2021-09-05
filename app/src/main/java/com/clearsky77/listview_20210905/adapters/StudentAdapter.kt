0461c3ababf394fc38b94aef6caef35a435288a1
clearsky77 <clearsky3264@gmail.com>
2021-09-05 14:34:37 +0900

    java util을 이용한 나이계산까지

--- app/src/main/java/com/clearsky77/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/clearsky77/listview_20210905/adapters/StudentAdapter.kt
@@ -8,17 +8,20 @@ import android.widget.ArrayAdapter
 import android.widget.TextView
 import com.clearsky77.listview_20210905.R
 import com.clearsky77.listview_20210905.datas.StudentData
+import java.util.*
+import kotlin.collections.ArrayList
 
 class StudentAdapter(
     val mContext: Context, //어느 화면에
     val resId: Int, //
-    val mList: ArrayList<StudentData> ) : ArrayAdapter<StudentData>(mContext, resId, mList){ //:는 상속이다
+    val mList: ArrayList<StudentData>
+) : ArrayAdapter<StudentData>(mContext, resId, mList) { //:는 상속이다
 
     // xml을 가져와서 코틀린단에서 활용하도록 가져와주는 도구 생성
     val mInflater = LayoutInflater.from(mContext)
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var tempRow = convertView //임시로 한 줄에 해당하는 것을 만들어 줌.
-        if (tempRow == null){
+        if (tempRow == null) {
             tempRow = mInflater.inflate(R.layout.student_list_item, null) // R이라는 것은 res 폴더다!
         }
         val row = tempRow!!
@@ -29,9 +32,12 @@ class StudentAdapter(
         val addressTxt = row.findViewById<TextView>(R.id.addressTxt)
         val ageTxt = row.findViewById<TextView>(R.id.ageTxt)
         nameTxt.text = data.name
-        addressTxt.text = data.name
-        //addressTxt.text = data.name
+        addressTxt.text = data.address
 
-                return row
+//      나이 계산
+        val age = Calendar.getInstance().get(Calendar.YEAR) - data.birthYear +1
+        ageTxt.text = "(${age}세)"
+
+        return row
     }
 }
