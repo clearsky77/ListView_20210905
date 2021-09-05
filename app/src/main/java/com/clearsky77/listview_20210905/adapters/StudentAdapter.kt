6802e117a4c6f04d582b213877ea092b0fd8f311
clearsky77 <clearsky3264@gmail.com>
2021-09-05 12:35:53 +0900

    StudentAdapter를 실제 사용할 때 필요한 재료를 요청. - 왜? ArrayAdapter에게 넘겨줘야 동작되기 때문

--- app/src/main/java/com/clearsky77/listview_20210905/adapters/StudentAdapter.kt
+++ app/src/main/java/com/clearsky77/listview_20210905/adapters/StudentAdapter.kt
@@ -1,7 +1,11 @@
 package com.clearsky77.listview_20210905.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.clearsky77.listview_20210905.datas.StudentData
 
-class StudentAdapter : ArrayAdapter<StudentData>(){ //:상속이다
+class StudentAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<StudentData> ) : ArrayAdapter<StudentData>(){ //:는 상속이다
 }
